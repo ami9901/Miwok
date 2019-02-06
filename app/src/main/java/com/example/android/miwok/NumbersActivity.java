59e0ebdefd9dc19e6bbe30f7d600693b0fe127c6
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2019-02-07 01:52:42 +0530

    added custom ArrayAdapter class

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,7 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -25,27 +30,24 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String [] words= new String[10];
-        words[0]="one";
-        words[1]="two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
 
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
-        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
-        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
-        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
-        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
-        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
-        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        ArrayList<Word> words= new ArrayList<>();
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'accha"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this,words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
