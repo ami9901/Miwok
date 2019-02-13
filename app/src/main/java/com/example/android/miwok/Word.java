465188d7c3c9166e7303f21af7affa02b3fbd55e
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2019-02-13 21:22:45 +0530

    images are added

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,10 +3,17 @@ package com.example.android.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId=NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED=-1;
 
     public Word(String mDefaultTranslation,String mMiwokTranslation){
         this.mDefaultTranslation=mDefaultTranslation;
         this.mMiwokTranslation=mMiwokTranslation;
+    }
+    public Word(String mDefaultTranslation,String mMiwokTranslation,int ImageResourceId){
+        this.mDefaultTranslation=mDefaultTranslation;
+        this.mMiwokTranslation=mMiwokTranslation;
+        mImageResourceId=ImageResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -14,5 +21,9 @@ public class Word {
     }
     public String getMiwokTranslation(){
         return mMiwokTranslation;
+    }
+    public int getImageResourceId(){ return mImageResourceId;}
+    public boolean hasImage(){
+        return mImageResourceId!=NO_IMAGE_PROVIDED;//checks if image is provided or not if != return true i.e image is provided
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,6 +35,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         miwokTextView.setText(local_word.getMiwokTranslation());
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_textView);
         defaultTextView.setText(local_word.getDefaultTranslation());
+        ImageView image =(ImageView)listItemView.findViewById(R.id.image);
+        image.setImageResource(local_word.getImageResourceId());
 
         return listItemView;
 }
